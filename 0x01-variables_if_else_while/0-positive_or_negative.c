59888dae35a8277c4006a9cf6719cd6a0387bdab
walaebella <teslawal369@gmail.com>
2023-03-17 08:59:48 -0700

    is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,12 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	int n;
