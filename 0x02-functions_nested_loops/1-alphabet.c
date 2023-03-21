371ca198f68b7c9a18a337321e1d91e6e11446db
walaebella <teslawal369@gmail.com>
2023-03-21 07:46:32 -0700

    print alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet - lowercase
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	_putchar(j);
+	_putchar('\n');
+}
