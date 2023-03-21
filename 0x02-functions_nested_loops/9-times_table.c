7e617aa8668fa39d12564545bcd45e8d90a0f835
walaebella <teslawal369@gmail.com>
2023-03-21 13:00:55 -0700

    9 times table c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
-				_putchar( u + '0');
+				_putchar(u + '0');
 			}
 			else 
 			{
@@ -29,7 +29,7 @@ void times_table(void)
 					_putchar(32);
 					_putchar(32);
 				}
-				_putchar( z + '0');
+				_putchar(z + '0');
 			}
 		}
 		_putchar('\n');
