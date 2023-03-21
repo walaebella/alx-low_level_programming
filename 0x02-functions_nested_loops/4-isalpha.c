13321202f35dcd0a3fa8bb4e0dec09410e4d9635
walaebella <teslawal369@gmail.com>
2023-03-21 09:27:56 -0700

    is lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks if a letter is lowercase or uppercase
+ *
+ * @c: check character
+ *
+ * Return: 1 if is alphabet otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c >= 90))
+		return (1);
+	else
+		return (0);
+}
