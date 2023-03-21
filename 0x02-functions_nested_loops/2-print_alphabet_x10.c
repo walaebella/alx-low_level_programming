04be10643c8eb4abf2eb5ce529708204efc68bae
walaebella <teslawal369@gmail.com>
2023-03-21 07:59:13 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function print the alphabet 10 times
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
