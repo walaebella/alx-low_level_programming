1dbc18b532f82823cc91389d233fd4349e304a5a
walaebella <teslawal369@gmail.com>
2023-03-21 17:15:44 -0700

    isalphaa

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,6 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -12,5 +11,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+int _isalpha(int c);
 
 #endif
