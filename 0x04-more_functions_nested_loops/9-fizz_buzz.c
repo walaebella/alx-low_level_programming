0af9e7f98bfbca3ccbe77d142dd26bd0cf13d06e
walaebella <teslawal369@gmail.com>
2023-03-23 15:36:44 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * main - Fizz Buzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1, j = 100;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d",i);
+		putchar(i == j ? '\n' : ' ');
+		i++;
+	}
+	return (0);
+}
