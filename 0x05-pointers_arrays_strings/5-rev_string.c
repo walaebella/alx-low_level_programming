c8ebf181e569905f74bf94a4656d906cfc124299
walaebella <teslawal369@gmail.com>
2023-03-27 08:44:56 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char k;
+
+	while (*s)
+	{
+		s++;
+		i++;
+	}
+	for (i = 0; i < j; i++)
+		s--;
+	for (i = 0; i < (j / 2);  i++)
+	{
+		int p = i - 1;
+		k = s[p - i];
+		s[i] = k;
+	}
+}
