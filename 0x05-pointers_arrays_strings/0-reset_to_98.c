e07a3af6d7ef89150da1e0e09e98a3f70d49e49d
walaebella <teslawal369@gmail.com>
2023-03-27 06:22:57 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value it point to 98
+ * @n: the parameter to modify
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef main
+
+void reset_to_98(int *n);
+
+
+
+#define main
+#endif
