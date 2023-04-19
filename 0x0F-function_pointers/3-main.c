7ec9643cbb3eb5847e2348ea7d719e09ea625399
walaebella <teslawal369@gmail.com>
2023-04-19 15:10:21 -0700

    perfoms simple operators

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include"3-calc.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - checks the code
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operation = get_op_func(argv[2]);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", operation(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include"3-calc.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substaractes two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplus two numbers
+ * @a: first number
+ * @b: second number
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - calcul modulo of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
